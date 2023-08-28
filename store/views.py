88f54e11d73b39f879b26df2a1b04f67c78eb57a
Biruk Kebede <biruk.key@gmail.com>
2023-08-28 17:40:03 +0100

    Adding product info route and template

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Category, Product
+from django.shortcuts import get_object_or_404
 
 def store(request):
     products = Product.objects.all()
@@ -9,4 +10,10 @@ def store(request):
 def category(_):
     categories = Category.objects.all()
     return {'categories': categories}
+
+
+def product_info(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {'product': product}
+    return render(request, 'store/product-info.html', context)
 
