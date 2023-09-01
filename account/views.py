b7decbdf99e5ceca42959292e25afffa8d472fa1
Biruk Kebede <biruk.key@gmail.com>
2023-09-01 21:59:33 +0100

    Generating user authentication token

--- account/views.py
+++ account/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.sites.shortcuts import get_current_site
 from .forms import UserRegistrationForm
+from .token import user_tokenizer_generate
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 
 def register(request):
@@ -7,8 +12,22 @@ def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('store')  # TODO - redirect to login page when created
+            user = form.save()
+            user.is_active = False
+            user.save()
+            # Email verification
+            current_site = get_current_site(request)
+            subject = 'Account Verification Email'
+            message = render_to_string('registration/email-verification.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),
+            })
+            user.email_user(subject, message)
+            return redirect(
+                'email-verification-sent'
+            )
 
     return render(request, 'registration/register.html', {'form': form})
 
