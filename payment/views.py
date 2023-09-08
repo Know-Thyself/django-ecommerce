5fbdddac330489738b7d5f14345c3e64c82d6b0f
Biruk Kebede <biruk.key@gmail.com>
2023-09-09 00:44:53 +0100

    Refined checkout conditions

--- payment/views.py
+++ payment/views.py
@@ -24,32 +24,29 @@ def checkout(request):
     except ShippingAddress.DoesNotExist:
         # If the user is a guest or authenticated but did not register their shipping address
         shipping_address = None
-    # The form will be prefilled depending on the above conditions
+    # The form will either be prefilled or empty depending on the above conditions
     form = ShippingForm(instance=shipping_address)
 
     if request.method == 'POST':
         form = ShippingForm(request.POST, instance=shipping_address)
         if form.is_valid():
-            # shipping_address = form.save()
-            # shipping_address.save()
+            for key, val in request.POST.items():
+                if key != 'csrfmiddlewaretoken':
+                    order_detail[key] = val
             cart = Cart(request)
             total_price = cart.get_total_price()
             order_detail['amount_paid'] = total_price
-            if request.user.is_authenticated and shipping_address != None:
-                for key, val in shipping_address.__dict__.items():
-                    if key != '_state' and key != 'user_id' and key != 'id':
-                        order_detail[key] = val
+            if request.user.is_authenticated:
                 order_detail['user'] = request.user
                 order = Order.objects.create(**order_detail)
                 order_id = order.pk
                 for item in cart:
                     OrderedItem.objects.create(order_id=order_id, product=item['product'], quantity=item['quantity'], price=item['price'], user=request.user)
             else:
-                for key, val in request.POST.items():
-                    if key != 'csrfmiddlewaretoken':
-                        order_detail[key] = val
                 order = Order.objects.create(**order_detail)
-            order.save()
+                order_id = order.pk
+                for item in cart:
+                    OrderedItem.objects.create(order_id=order_id, product=item['product'], quantity=item['quantity'], price=item['price'])
 
             return redirect('dashboard') # temp
 
