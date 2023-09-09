0e53c447f1e8b2c4ca262f953a6bc23a566d626b
Biruk Kebede <biruk.key@gmail.com>
2023-09-09 11:14:45 +0100

    Database migration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -91,9 +91,10 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        "default": dj_database_url.parse(environ.get("DATABASE_URL"))
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'default': dj_database_url.parse(environ.get('DATABASE_URL')),
     }
 }
 
