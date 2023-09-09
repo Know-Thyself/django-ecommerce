56f2f0ae46499b9b1cef6ae28963d7af096bb21d
Biruk Kebede <biruk.key@gmail.com>
2023-09-09 10:45:32 +0100

    Deployment error fix

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sustainability-ecommerce.onrender.com']
 
 
 # Application definition
